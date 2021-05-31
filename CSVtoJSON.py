9381468cfc918af195a66e61be96a25ae0f4b66d
AVlasova <vlasaleksa@gmail.com>
2021-05-31 20:23:47 +0300

    split to map reduce func

--- CSVtoJSON.py
+++ CSVtoJSON.py
@@ -14,19 +14,29 @@ def csv_to_json(csvFilePath, jsonFilePath):
     # Open a csv reader called DictReader
     with open(csvFilePath, encoding='UTF-8') as csvFile:
         csvReader = csv.DictReader(csvFile, dialect='excel-tab')
-
-        # Convert each row into a dictionary
-        # and add it to data
-        for rows in csvReader:
-            # Assuming a column named 'No' to
-            # be the primary key
-            key = rows['index']
-            data[key] = rows
+        data = map_csv(csvReader)
 
     # Open a json writer, and use the json.dumps()
     # function to dump data
     with open(jsonFilePath, 'w', encoding='UTF-8') as jsonFile:
-        jsonFile.write(json.dumps(data, indent=4, ensure_ascii=False))
+        jsonFile.write(reduce_csv(data))
+
+
+def reduce_csv(data):
+    # TODO write to file one-by-one
+    return json.dumps(data, indent=4, ensure_ascii=False)
+
+
+def map_csv(input_dict):
+    data = {}
+    # Convert each row into a dictionary
+    # and add it to data
+    for rows in input_dict:
+        # Assuming a column named 'No' to
+        # be the primary key
+        key = rows['index']
+        data[key] = rows
+    return data
 
 
 # Call the make_json function
